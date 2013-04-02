76df13abce1419ca830afb9538e68d28fc5cae68
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-04-02 13:58:57 -0400

    Okay, infinite scroll works now.

--- www/js/app-tumblr.js
+++ www/js/app-tumblr.js
@@ -2,12 +2,21 @@ $(document).ready(function(){
     $form = $('#project-form');
     $modal_btn = $('#modal-btn');
     $project_hdr = $form.find('.hdr');
-    
+
     $modal_btn.click(function() {
         $form.show();
     });
-    
+
     $project_hdr.click(function() {
         $form.hide();
     });
+
+    $('.post').disqusCommentCount();
 });
+
+$.fn.disqusCommentCount = function() {
+    var scriptURL = 'http://disqus.com/forums/whatthefridge/count.js';
+    $.getScript(scriptURL);
+
+    return this;
+};
