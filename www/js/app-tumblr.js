2ff41f13e6ecd3804affcc052e8a3954c2ac3b82
Alyson Hurt <ahurt@npr.org>
2013-04-02 18:36:53 -0400

    more fun with modals #51

--- www/js/app-tumblr.js
+++ www/js/app-tumblr.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
     $form = $('#project-form');
+    $modal_bg = $('.modal-bg');
     $modal_btn = $('#modal-btn');
     $project_hdr = $form.find('.hdr');
-
+    
+    function toggle_header() {
+        $form.toggle();
+        $modal_bg.toggle();
+    }
+    
     $modal_btn.click(function() {
-        $form.show();
+        toggle_header();
     });
-
     $project_hdr.click(function() {
-        $form.hide();
+        toggle_header();
     });
-
+    $modal_bg.click(function() {
+        toggle_header();
+    });
+    
 });
